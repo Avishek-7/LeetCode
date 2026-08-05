7b45f67a6f1a87166261b4b3598b9cd599a9d5cd
Avishek Kumar Yadav <84432588+Avishek-7@users.noreply.github.com>
2026-08-05 21:43:07 +0530

    Time: 8 ms (30.28%), Space: 22.6 MB (18.93%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,21 +6,32 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        def inorder_traversal(node):
-            if not node: 
-                return []
-            return inorder_traversal(node.left) + [node.val] + inorder_traversal(node.right)
+        seen = set()
 
-        nums = inorder_traversal(root)
-        left, right = 0, len(nums) - 1
-
-        while left < right:
-            current_sum = nums[left] + nums[right]
-            if current_sum == k:
+        def dfs(node):
+            if not node:
+                return False
+            
+            if k-node.val in seen:
                 return True
-            elif current_sum < k:
-                left += 1
-            else:
-                right -= 1
-        return False
+            seen.add(node.val)
+            return dfs(node.left) or dfs(node.right)
+        return dfs(root)
+        # def inorder_traversal(node):
+        #     if not node: 
+        #         return []
+        #     return inorder_traversal(node.left) + [node.val] + inorder_traversal(node.right)
+
+        # nums = inorder_traversal(root)
+        # left, right = 0, len(nums) - 1
+
+        # while left < right:
+        #     current_sum = nums[left] + nums[right]
+        #     if current_sum == k:
+        #         return True
+        #     elif current_sum < k:
+        #         left += 1
+        #     else:
+        #         right -= 1
+        # return False
         
